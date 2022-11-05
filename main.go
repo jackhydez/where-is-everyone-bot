295c974dc30bc507a44f785399adde0b4d9b94e9
jackhydez <pash.ok92@yandex.ru>
2022-11-06 02:44:15 +0300

    change quiz -> poll

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 
 			if msg.Text == "+" || msg.Text == "-" {
-				msg.Text = "ты чё мля с калькулятора, сцука!!!"
+				msg.Text = "ты чё мля с калькулятора, сцука!"
 				msg.ReplyToMessageID = update.Message.MessageID
 				bot.Send(msg)
 			}
@@ -101,8 +101,8 @@ func main() {
 							ChatID: update.Message.Chat.ID,
 						},
 						Question: msg.Text,
-						Type:     "quiz",
-						Options: []string{"да", "нет"},
+						Type:     "regular",
+						Options: []string{"да", "нет", "пукнуть"},
 						CorrectOptionID: 0,
 					}
 
