2e4c16fc207e44e8e60f42916942ba23fdc1c407
jackhydez <pash.ok92@yandex.ru>
2022-11-06 01:27:35 +0300

    test build jenkins number 0

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func main() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 
 			if msg.Text == "+" || msg.Text == "-" {
-				msg.Text = "ты чё мля с калькулятора, сцука!?1"
+				msg.Text = "ты чё мля с калькулятора, сцука!?11"
 				msg.ReplyToMessageID = update.Message.MessageID
 				bot.Send(msg)
 			}
